05a8f6dc0d8831beac63210c6dfd743ee8c3fe0b
H. Chase Stevens <chase@chasestevens.com>
2017-04-26 20:07:20 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+ 
+setup(
+    name='minmax',
+    packages=[],
+    version='0.0.0',
+    description='A command line optimization utility',
+    license='MIT',
+    author='H. Chase Stevens',
+    author_email='chase@chasestevens.com',
+    url='https://github.com/hchasestevens/minmax',
+    install_requires=[],
+    entry_points={
+        'console_scripts': []
+    },
+    keywords='minimize maximize optimization',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Utilities',
+    ]
+)
